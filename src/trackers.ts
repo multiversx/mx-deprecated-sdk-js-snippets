50ab8ce4029f9af60a8a3e76898be551bd7cd4d0
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2022-03-21 09:02:24 +0200

    Trackers: TBD.

--- src/trackers.ts
+++ src/trackers.ts
@@ -1,70 +1,26 @@
-import { AbiRegistry, Address, Balance, Code, CodeMetadata, DeployArguments, GasLimit, IInteractionChecker, Interaction, IProvider, SmartContract, SmartContractAbi, SmartContractResults, StrictChecker, TokenOfAccountOnNetwork, Transaction, TypedValue } from "@elrondnetwork/erdjs";
-import { TransactionOnNetwork } from "@elrondnetwork/erdjs/out/transactionOnNetwork";
+import { Address, Interaction, IProvider, SmartContract, TokenOfAccountOnNetwork } from "@elrondnetwork/erdjs";
 import { IAccountSnapshotWithinStorage, IStorage, ITestSession, IUser } from "./interfaces";
 
 
+// TBD
 export class InteractionsTracker {
     private readonly scope: string;
     private readonly storage: IStorage;
     private readonly proxy: IProvider;
-    private readonly checker: IInteractionChecker;
     public readonly contract: SmartContract;
 
     constructor(session: ITestSession, contract: SmartContract) {
         this.scope = session.scope;
         this.storage = session.storage;
         this.proxy = session.proxy;
-        this.checker = new StrictChecker();
         this.contract = contract;
     }
 
-    async doDeploy(
-        deployer: IUser,
-        pathToWasm: string,
-        deployArguments: {
-            codeMetadata?: CodeMetadata;
-            initArguments?: TypedValue[];
-            value?: Balance;
-            gasLimit: GasLimit;
-        }): Promise<Address> {
-        let code = await Code.fromFile(pathToWasm);
-
-        let transaction = this.contract.deploy({
-            code: code,
-            codeMetadata: deployArguments.codeMetadata,
-            initArguments: deployArguments.initArguments,
-            value: deployArguments.value,
-            gasLimit: deployArguments.gasLimit,
-        });
-
-        transaction.setNonce(deployer.account.getNonceThenIncrement());
-
-        await deployer.signer.sign(transaction);
-        await transaction.send(this.proxy);
-        await transaction.awaitExecuted(this.proxy);
-
-        console.log(`DefaultInteractor.doDeploy(): transaction = ${transaction.getHash()}, contract = ${this.contract.getAddress()}`);
-        return this.contract.getAddress();
-    }
-
-    async runQuery(user: IUser, interaction: Interaction, caller?: Address): Promise<{ firstValue: TypedValue, values: TypedValue[] }> {
-        this.checker.checkInteraction(interaction);
-
-        let query = interaction.buildQuery();
-        query.caller = caller || user.signer.getAddress();
-        let response = await this.proxy.queryContract(query);
-        let bundle = interaction.interpretQueryResponse(response);
-        let values = bundle.values;
-        return { firstValue: values[0], values: values };
-    }
-
-    async runInteraction(user: IUser, interaction: Interaction): Promise<{ transactionOnNetwork: TransactionOnNetwork, contractResults: SmartContractResults }> {
-        this.checker.checkInteraction(interaction);
-
-        let functionName = interaction.getExecutingFunction().name;
+    async trackInteraction(user: IUser, interaction: Interaction): Promise<void> {
+        let functionName = interaction.getFunction().name;
         let args = interaction.getArguments();
         let argsPlain = args.map(arg => arg.valueOf());
-        console.debug(`DefaultInteractor.runInteraction(): call = ${functionName}(${argsPlain.join(", ")})`);
+        console.debug(`InteractionsTracker.trackInteraction(): call = ${functionName}(${argsPlain.join(", ")})`);
 
         let interactingParticipants: Address[] = [
             user.address,
@@ -83,21 +39,7 @@ export class InteractionsTracker {
 
         let snapshotsAfterInteraction = await this.takeSnapshotsOfAccounts(interactingParticipants);
 
-        let contractResults = transactionOnNetwork.getSmartContractResults();
-        let immediateResult = contractResults.getImmediate();
-        let output: any;
-
-        if (immediateResult.isSuccess()) {
-            immediateResult.setEndpointDefinition(interaction.getEndpoint());
-            output = immediateResult.outputTyped();
-        } else {
-            output = {
-                returnCode: immediateResult.getReturnCode(),
-                returnMessage: immediateResult.getReturnMessage()
-            };
-
-            console.error("Contract error:", output);
-        }
+        // TBD
 
         let transfers = {
             value: interaction.getValue().valueOf(),
@@ -121,7 +63,7 @@ export class InteractionsTracker {
             hyperblockNonce: transactionOnNetwork.hyperblockNonce,
             input: argsPlain,
             transfers: transfers,
-            output: output
+            output: {}
         });
 
         for (const snapshot of snapshotsBeforeInteraction) {
@@ -133,8 +75,6 @@ export class InteractionsTracker {
             snapshot.takenAfterInteraction = interactionReference;
             await this.storage.storeAccountSnapshot(this.scope, snapshot);
         }
-
-        return { transactionOnNetwork, contractResults };
     }
 
     private async takeSnapshotsOfAccounts(addresses: Address[]): Promise<IAccountSnapshotWithinStorage[]> {
@@ -166,13 +106,4 @@ export class InteractionsTracker {
             nonce: token.nonce.valueOf()
         };
     }
-}
-
-export async function createSmartContract(pathToAbi: string, address?: Address) {
-    let registry = await AbiRegistry.load({ files: [pathToAbi] });
-    // We only have single-interface ABIs anyway.
-    let contractInterface = registry.interfaces[0].name;
-    let abi = new SmartContractAbi(registry, [contractInterface]);
-    let contract = new SmartContract({ address: address, abi: abi });
-    return contract;
 }
