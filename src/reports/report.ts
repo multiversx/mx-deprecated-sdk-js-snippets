69b3e90dcad704c95fd592337c4830110305025f
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2022-05-11 21:07:09 +0300

    Reports / WIP.

--- src/reports/report.ts
+++ src/reports/report.ts
@@ -2,7 +2,7 @@ import ejs from "ejs";
 import { readFileSync, writeFileSync } from "fs";
 import { BreadcrumbTypeAddress, BreadcrumbTypeToken } from "../constants";
 import { resolvePath } from "../filesystem";
-import { IBreadcrumbRecord, IReportingConfig, IStorage } from "../interface";
+import { IAccountSnapshotRecord, IBreadcrumbRecord, IInteractionRecord, IReportingConfig, IStorage } from "../interface";
 
 export class Report {
     private readonly config: IReportingConfig;
@@ -17,6 +17,7 @@ export class Report {
 
     async prepare() {
         const breadcrumbs = await this.storage.loadBreadcrumbs();
+        const interactions = await this.storage.loadInteractions();
 
         this.model.breadcrumbs = breadcrumbs
             .map(e => new ArbitraryBreadcrumbModel(e));
@@ -28,6 +29,10 @@ export class Report {
         this.model.tokens = breadcrumbs
             .filter(e => e.type == BreadcrumbTypeToken)
             .map(e => new TokenBreadcrumbModel(e));
+
+        this.model.interactions = interactions
+            .sort(e => e.id)
+            .map(e => new InteractionModel(e, this.config));
     }
 
     async generate(tag?: string) {
@@ -56,6 +61,7 @@ class ReportModel {
     addresses: AddressBreadcrumbModel[] = [];
     tokens: TokenBreadcrumbModel[] = [];
     breadcrumbs: ArbitraryBreadcrumbModel[] = [];
+    interactions: InteractionModel[] = [];
 }
 
 class AddressBreadcrumbModel {
@@ -95,8 +101,77 @@ class ArbitraryBreadcrumbModel {
     constructor(breadcrumb: IBreadcrumbRecord) {
         this.name = breadcrumb.name;
         this.type = breadcrumb.type;
-        this.payload = JSON.stringify(breadcrumb.payload, null, 4);
+        this.payload = stringifyObject(breadcrumb.payload);
         this.tag = breadcrumb.correlationTag;
     }
 }
 
+class InteractionModel {
+    id: number;
+    timestamp: string;
+    transaction: string;
+    transactionHref: string;
+    action: string;
+    user: string;
+    userHref: string;
+    contract: string;
+    contractHref: string;
+    input: string;
+    transfers: string;
+    output: string;
+    tag: string;
+
+    snapshotsBefore: AccountSnapshotModel[] = [];
+    snapshotsAfter: AccountSnapshotModel[] = [];
+
+    constructor(interaction: IInteractionRecord, config: IReportingConfig) {
+        const user = interaction.userAddress.bech32();
+        const contract = interaction.contractAddress.bech32();
+        const transaction = interaction.transactionHash.toString();
+
+        this.id = interaction.id;
+        this.timestamp = interaction.timestamp;
+        this.action = interaction.action;
+        this.user = getAddressShorthand(user);
+        this.userHref = `${config.explorerUrl}/accounts/${user}`;
+        this.contract = getAddressShorthand(contract);
+        this.contractHref = `${config.explorerUrl}/accounts/${user}`;
+        this.transaction = getHashShorthand(transaction);
+        this.transactionHref = `${config.explorerUrl}/transactions/${transaction}`;
+
+        this.input = stringifyObject(interaction.input);
+        this.transfers = stringifyObject(interaction.transfers);
+        this.output = stringifyObject(interaction.output);
+        this.tag = interaction.correlationTag;
+    }
+}
+
+class AccountSnapshotModel {
+    address: string;
+    nonce: number;
+    balance: string;
+    fungibleTokens: string;
+    nonFungibleTokens: string;
+
+    constructor(snapshot: IAccountSnapshotRecord) {
+        this.address = snapshot.address.bech32();
+        this.nonce = snapshot.nonce
+        this.balance = snapshot.balance.toString();
+        this.fungibleTokens = stringifyObject(snapshot.fungibleTokens);
+        this.nonFungibleTokens = stringifyObject(snapshot.fungibleTokens);
+    }
+}
+
+function stringifyObject(obj: any) {
+    return JSON.stringify(obj, null, 4);
+}
+
+function getAddressShorthand(address: string) {
+    let shorthand = `${address.substring(0, 12)}...${address.substring(address.length - 8)}`;
+    return shorthand;
+}
+
+function getHashShorthand(hash: string) {
+    let shorthand = `${hash.substring(0, 4)}...${hash.substring(hash.length - 4)}`;
+    return shorthand;
+}
