6563d5ac5bd99ee8a021f5b004393eeff0c94cbb
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-01-19 00:01:38 +0200

    Refactor, improve snippet.

--- src/integration-tests/counter/counter.spec.ts
+++ src/integration-tests/counter/counter.spec.ts
@@ -3,7 +3,6 @@ import { assert } from "chai";
 import { createAirdropService } from "../../airdrop";
 import { FiveMinutesInMilliseconds } from "../../constants";
 import { ITestSession, ITestUser } from "../../interface";
-import { INetworkProvider } from "../../interfaceOfNetwork";
 import { TestSession } from "../../session";
 import { createESDTInteractor } from "../../system/esdtInteractor";
 import { createInteractor } from "./counterInteractor";
@@ -12,15 +11,11 @@ describe("counter snippet", async function () {
     this.bail(true);
 
     let session: ITestSession;
-    let provider: INetworkProvider;
-    let whale: ITestUser;
     let owner: ITestUser;
     let alice: ITestUser;
 
     this.beforeAll(async function () {
         session = await TestSession.load("devnet", __dirname);
-        provider = session.networkProvider;
-        whale = session.users.getUser("whale");
         owner = session.users.getUser("whale");
         alice = session.users.getUser("alice");
         await session.syncNetworkConfig();
@@ -29,17 +24,18 @@ describe("counter snippet", async function () {
     it("issue counter token", async function () {
         this.timeout(FiveMinutesInMilliseconds);
 
-        let interactor = await createESDTInteractor(session);
+        const interactor = await createESDTInteractor(session);
         await session.syncUsers([owner]);
-        let token = await interactor.issueFungibleToken(owner, { name: "COUNTER", ticker: "COUNTER", decimals: 0, supply: "100000000" });
+        const token = await interactor.issueFungibleToken(owner, { name: "COUNTER", ticker: "COUNTER", decimals: 0, supply: "100000000" });
         await session.saveToken({ name: "counterToken", token: token });
+        await session.save();
     });
 
     it("airdrop counter token", async function () {
         this.timeout(FiveMinutesInMilliseconds);
 
-        let token = await session.loadToken("counterToken");
-        let payment = TokenPayment.fungibleFromAmount(token.identifier, "100", token.decimals);
+        const token = await session.loadToken("counterToken");
+        const payment = TokenPayment.fungibleFromAmount(token.identifier, "100", token.decimals);
         await session.syncUsers([owner]);
         await createAirdropService(session).sendToEachUser(owner, [alice], [payment]);
     });
@@ -49,12 +45,13 @@ describe("counter snippet", async function () {
 
         await session.syncUsers([owner]);
 
-        let interactor = await createInteractor(session);
-        let { address, returnCode } = await interactor.deploy(owner, 42);
+        const interactor = await createInteractor(session);
+        const { address, returnCode } = await interactor.deploy(owner, 42);
 
         assert.isTrue(returnCode.isSuccess());
 
         await session.saveAddress({ name: "counter", address: address });
+        await session.save();
     });
 
     it("increment with single ESDT transfer", async function () {
@@ -62,17 +59,19 @@ describe("counter snippet", async function () {
 
         await session.syncUsers([owner, alice]);
 
-        let contractAddress = await session.loadAddress("counter");
-        let token = await session.loadToken("counterToken");
-        let interactor = await createInteractor(session, contractAddress);
+        const contractAddress = await session.loadAddress("counter");
+        const token = await session.loadToken("counterToken");
+        const interactor = await createInteractor(session, contractAddress);
 
-        let payment = TokenPayment.fungibleFromAmount(token.identifier, "10", token.decimals);
+        const payment = TokenPayment.fungibleFromAmount(token.identifier, "10", token.decimals);
 
         // Intra-shard
         interactor.incrementWithSingleESDTTransfer(owner, 1, payment);
 
         // Cross-shard
         interactor.incrementWithSingleESDTTransfer(alice, 1, payment);
+
+        await session.save();
     });
 
     it("increment with multi transfer", async function () {
@@ -80,17 +79,19 @@ describe("counter snippet", async function () {
 
         await session.syncUsers([owner, alice]);
 
-        let contractAddress = await session.loadAddress("counter");
-        let token = await session.loadToken("counterToken");
-        let interactor = await createInteractor(session, contractAddress);
+        const contractAddress = await session.loadAddress("counter");
+        const token = await session.loadToken("counterToken");
+        const interactor = await createInteractor(session, contractAddress);
 
-        let payment = TokenPayment.fungibleFromAmount(token.identifier, "10", token.decimals);
+        const payment = TokenPayment.fungibleFromAmount(token.identifier, "10", token.decimals);
 
         // Intra-shard
         interactor.incrementWithMultiTransfer(owner, 1, payment);
 
         // Cross-shard
         interactor.incrementWithSingleESDTTransfer(alice, 1, payment);
+
+        await session.save();
     });
 
     it("destroy session", async function () {
